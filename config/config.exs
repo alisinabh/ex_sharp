b2e8f43c778e52f9ab9af2d4668991add279abe1
Sam Schneider <sds6065@gmail.com>
2016-03-27 15:55:51 -0400

    comment out config

--- config/config.exs
+++ config/config.exs
@@ -28,4 +28,4 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
-config :ex_sharp, csx_path: Path.expand("../priv/ExampleScript.csx", __DIR__)
+#config :ex_sharp, csx_path: Path.expand("../priv/ExampleScript.csx", __DIR__)
